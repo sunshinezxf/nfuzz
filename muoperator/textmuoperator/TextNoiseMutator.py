4f31ee61bfa8aebfa55efadeaa9db601e424eda2
ffffrank <630656007@qq.com>
2020-11-03 19:52:36 +0800

    text mutator init

---
+++ muoperator/textmuoperator/TextNoiseMutator.py
@@ -0,0 +1,17 @@
+import jpype
+from jpype import *
+from muoperator.Mutator import Mutator
+
+
+class TextNoiseMutator(Mutator):
+    """
+        文本加噪父类，加噪方法：
+            替换；
+            插入；
+            交换；
+            删除；
+    """
+    def mutate(self, seed):
+
+        pass
+
